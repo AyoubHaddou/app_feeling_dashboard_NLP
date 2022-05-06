bb7ad0d8e1931604e78d92af4e8f0f5e09a2a09e
VAlGrus <grusonvalent@gmail.com>
2022-05-06 11:53:40 +0200

    maj

--- app.py
+++ app.py
@@ -4,45 +4,44 @@ import streamlit as st
 import streamlit_authenticator as stauth 
 
 
+## Home 
 
+#st.header('BALBOA COACHING')
+#st.image ('motivation.jpeg')
 
-st.header('BALBOA COACHING')
-st.image ('motivation.jpeg')
-st.write ('"Toi, moi, personne ne frappe aussi fort que la vie"')
+#names = ['John Smith','Rebecca Briggs']
+#usernames = ['jsmith','rbriggs']
+#passwords = ['123','456']
 
-names = ['John Smith','Rebecca Briggs']
-usernames = ['jsmith','rbriggs']
-passwords = ['123','456']
+#hashed_passwords = stauth.Hasher(passwords).generate()
 
-hashed_passwords = stauth.Hasher(passwords).generate()
+#authenticator = stauth.Authenticate(names,usernames,hashed_passwords,
+#    'some_cookie_name','some_signature_key',cookie_expiry_days=30)
 
-authenticator = stauth.Authenticate(names,usernames,hashed_passwords,
-    'some_cookie_name','some_signature_key',cookie_expiry_days=30)
-
-name, authentication_status, username = authenticator.login('Login','main')
+#name, authentication_status, username = authenticator.login('Login','main')
 
 ## https://towardsdatascience.com/how-to-add-a-user-authentication-service-in-streamlit-a8b93bf02031 : authentification 
 
 
+### Admin 
+
+#st.sidebar.text('Nom : Balboa')
+#st.sidebar.text('Prénom : Rocky')
+#st.sidebar.selectbox('Que souhaitez vous faire ?', ['Suivis patient', 'Gestion clientèle', 'Statistique générale'])
+
+#st.header('BALBOA COACHING')
+#st.image ('motivation.jpeg')
 
 
 
-st.sidebar.image('profil.jpeg')
-st.sidebar.text('Nom : Balboa')
-st.sidebar.text('Prénom : Rocky')
-st.sidebar.text('Statut : Coach')
-st.sidebar.selectbox('Que souhaitez vous faire ?', ['Suivis patient', 'Gestion clientèle', 'Statistique générale'])
+#### Client 
+
+st.sidebar.text('Nom : Creed')
+st.sidebar.text('Prénom : Apollo')
+selection = st.sidebar.selectbox('Que souhaitez vous faire ?', ['Rédiger votre texte du jour', 'Modifier votre texte du jour', 'Consulter vos texte', 'Consulter vos progressions'])
 
 st.header('BALBOA COACHING')
 st.image ('motivation.jpeg')
-st.write ('"Toi, moi, personne ne frappe aussi fort que la vie"')
-
- 
-st.sidebar.image('creed.jpg')
-st.sidebar.text('Nom : Creed')
-st.sidebar.text('Prénom : Apollo')
-st.sidebar.text('Statut : Patient')
-selection = st.sidebar.selectbox('Que souhaitez vous faire ?', ['Rédiger votre texte du jour', 'Modifier votre texte du jour', 'Consulter vos texte', 'Consulter vos progressions'])
 
 if selection == 'Rédiger votre texte du jour':
     WordOfDay = st.text_area('Ecrivez votre texte du jour :')
