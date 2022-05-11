fff1d8e4d90fb98b9fd7d7953a386d30ea4d2599
VAlGrus <grusonvalent@gmail.com>
2022-05-11 14:03:50 +0200

    maj

--- database.py
+++ database.py
@@ -5,20 +5,21 @@ from numpy import insert, reciprocal
 import psycopg2
 import psycopg2.extras 
 from requests import delete
-import sqlite3
 
-hostname = 'localhost123'
-database = 'demo'
-username = 'mr_zen'
-pwd = 'rockybalboa'
-port_id = 5432
+CREATE DATABASE coach_db; 
 
+conn = psycopg2.connect(
+host = 'localhost'
+database = 'coach_db'
+username = 'mrzen'
+pwd = 'rockybalboa')
 
-cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor) 
+database.ini
 
+cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
-# création de la base de donnée
-create_script = """ CREATE TABLE utilisateur IF NOT EXIST ( 
+def create_db():
+    create_script = """ CREATE TABLE utilisateur IF NOT EXIST ( 
                     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,  
                     nom VARCHAR(100), 
                     prenom VARCHAR(100), 
@@ -31,31 +32,28 @@ create_script = """ CREATE TABLE utilisateur IF NOT EXIST (
                     Emotion_majoritaire VARCHAR(20), 
                     Statut VARCHAR(10)
                     ) """
-cur.execute(create_script)
-
-#supprimer élément de la base de donnée
-delete_script = 'DELETE FROM utilisateurs WHERE name = %s'
-cur.execute(delete_script)
-
-# Affichage pour admin
-cur.execute('SELECT * FROM utilisateur')
-for record in cur.fetchall():
-    print(record['nom'],record['prenom'],record['email'],record['data_naissance'],record['ville'],record['code_postal'],record['date_du_texte'],record['Texte_du_jour'],record['Emotion_majoritaire'],record['Statut'])
-    conn.commit()
-
-#Affichage utilisateur 
-cur.execute('SELECT')
-
-#Mise à jour utilisateur
-update_script = 'UPDATE utilisateur SET Texte_du_jour = %s WHERE Texte_du_jour = %s'
-cur.execute(update_script)
-
-#Insertion dans la base de donnée client 
-insert_texte = 'INSERT INTO utilisateur (Texte_du_jour) VALUES (%s)'
-cur.execute(insert_texte)
+    cur.execute(create_script)
 
 def insert_user(nom,prenom,record,email,data_naissance,ville,code_postal,date_du_texte,Texte_du_jour,Emotion_majoritaire,Statut):
-    # insertion dans la base de donnée admin  
     insert_script = f'INSERT INTO utilisateur VALUES {nom,prenom,record,email,data_naissance,ville,code_postal,date_du_texte,Texte_du_jour,Emotion_majoritaire,Statut}'
     conn.commit()
     cur.execute(insert_script)
+
+def delete_db_element(nom):
+    delete_script = f'DELETE FROM utilisateurs WHERE name = {nom}'
+    cur.execute(delete_script)
+
+def admin_display():
+    cur.execute('SELECT * FROM utilisateur')
+    conn.commit()
+       
+def user_display():
+    cur.execute('SELECT')
+
+def update_user(Texte_du_jour):
+    update_script = f'UPDATE utilisateur SET Texte_du_jour = {Texte_du_jour} WHERE Texte_du_jour = {Texte_du_jour}'
+    cur.execute(update_script)
+
+def day_text(Texte_du_jour):
+    insert_texte = f'INSERT INTO utilisateur (Texte_du_jour) VALUES {Texte_du_jour}'
+    cur.execute(insert_texte)
