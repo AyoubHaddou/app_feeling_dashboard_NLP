53c883cf10a02fa5e25bbe38efbe9efa1a34c768
ayoub <ayoubhaddou1@gmail.com>
2022-05-13 16:19:48 +0200

    db

--- app.py
+++ app.py
@@ -71,9 +71,9 @@ def update_user(Texte_du_jour):
     db.commit()
     db.close()
 
-def day_text(Texte_du_jour):
+def day_text(Texte_du_jourr):
     db, cur = login_db()
-    insert_texte = f'INSERT INTO utilisateur (Texte_du_jour) VALUES ({Texte_du_jour});'
+    insert_texte = f'INSERT INTO utilisateur (Texte_du_jour) VALUES ({Texte_du_jourr});'
     cur.execute(insert_texte)
     db.commit()
     db.close()
