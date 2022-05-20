6f490f0e9621fbc39d185bfef978adbc7f94fdaa
ayoub <ayoubhaddou1@gmail.com>
2022-05-20 13:19:49 +0200

    cleaning + text bdd

--- app.py
+++ app.py
@@ -7,102 +7,94 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine , update , func 
 from models import User, Text 
 import datetime 
-from function import predict_data
+from function import predict_data, make_engine_session, emotion_int
 import matplotlib.pyplot as plt
 
 @dataclass 
 class Page:
     config = st.set_page_config(layout="wide")
     col1, col2, col3 = st.columns(3)
-    selection = ""
+    sess = make_engine_session()
     info_dashboard = ""
-    sess = ""
+    side_selection = ""
+    current_user = ""
+    user_text = ""
     title = ""
     image_page = ""
     WordOfDay = ""
     actual_username = ""
-    current_user = ""
-    user_text = ""
 
 
-    def init_session(self):
+    def login_streamlit(self):
 
-        
-
-        engine = create_engine('sqlite:///feeling_db.sqlite3?check_same_thread=False')
-        Session = sessionmaker(bind=engine)
-        self.sess = Session()
         user_all = self.sess.query(User).all()
-        password_all = []
+        hash_password_all = []
         usernames_all = []
         names_all = []
-
         for user in user_all:
             coach_name = user.name
             names_all.append(coach_name)
             usernames_all.append(user.username)
-            password_all.append(user.password)
-     
+            hash_password_all.append(user.password)
 
         # Authentification    
-         
-        hash_password_all = stauth.Hasher(password_all).generate()
         authenticator = stauth.Authenticate(names_all, usernames_all,hash_password_all,
                                             'some_cookie_name','some_signature_key',cookie_expiry_days=0)
         authenticator.login('Login','main')
 
-
-
-    def session(self):
+    def instance_session(self):
 
         if (st.session_state['authentication_status'] == False) or (st.session_state['authentication_status'] == None) :
-            self.info_dashboard = st.sidebar.text('Free demo account : \nUsername : jsmith \nPassword : 123')
-            self.selection = st.sidebar.text('Please login')
             with self.col2:
                 self.image_page = st.image('coach.gif')
-
+            self.info_dashboard = st.sidebar.text('Free demo account : \nUsername : jsmith \nPassword : 123')
+            self.side_selection = st.sidebar.text('Please login')
             if st.session_state['authentication_status'] == False :
                 st.error('Please enter correct username and password')
 
-
         elif st.session_state['authentication_status']:
+            with self.col2:
+                self.image_page = st.image('motivation.jpeg')
             self.info_dashboard = st.sidebar.text('Welcome *%s*' % (st.session_state['name']))
-            self.selection = st.sidebar.selectbox('Que souhaitez vous faire ?', ['Rédiger votre texte du jour', 'Modifier votre texte du jour', 'Consulter vos texte', 'Consulter vos progressions'])
+            side_bar_admin = ['Rédiger votre texte du jour', 'Modifier votre texte du jour', 'Suivit des emotions', 'Ajouter un patient']
+            side_bar_user = ['Rédiger votre texte du jour', 'Modifier votre texte du jour', 'Suivit des emotions']
             self.actual_username = st.session_state['username']
             check_a = self.sess.query(User).filter_by( username = self.actual_username, is_coach=True).first()
             check_b = self.sess.query(User).filter_by( username = self.actual_username, is_coach=False).first()
             if check_a :
-                self.user_text = self.sess.query(Text).all()
                 self.current_user = check_a
+                self.user_text = self.sess.query(Text).all()
+                self.side_selection = st.sidebar.selectbox('Que souhaitez vous faire ?', side_bar_admin)
             elif check_b :
-                self.user_text = self.sess.query(Text).filter_by( user_id = check_b.id).all()
                 self.current_user = check_b
-            with self.col2:
-                self.image_page = st.image('motivation.jpeg')
+                self.user_text = self.sess.query(Text).filter_by( user_id = check_b.id).all()
+                self.side_selection = st.sidebar.selectbox('Que souhaitez vous faire ?', side_bar_user)
 
 
-    def display_content(self):
 
-        if self.selection == 'Rédiger votre texte du jour':
+    def add_content(self):
+
+        if self.side_selection == 'Rédiger votre texte du jour':
             self.title = st.title('Your day text')
             self.WordOfDay = st.text_area('Rédiger votre texte du jour')
             button = st.button('Publier')
             if button and self.WordOfDay:
-                last_date = [0]
+                date_now = datetime.datetime.now().day
+                last_post = [0]
                 if self.user_text:
                     for text in self.user_text :
-                        last_date.append(text.time_created.day)
-                date = datetime.datetime.now().day
-                if last_date[-1] != date:
+                        last_post.append(text.time_created.day)
+                if last_post[-1] != date_now:
                     text = Text(content=self.WordOfDay, emotion_predicted=predict_data(self.WordOfDay), user_id=self.current_user.id)
                     self.sess.add(text)
                     self.sess.commit()
-                    st.balloons()
                     st.success('Message du jour envoyé!')
                 else:
                     st.error('Text already send today. Go to modification space if you need it.')
 
-        elif self.selection == 'Modifier votre texte du jour':
+    def update_content(self):
+
+        if self.side_selection == 'Modifier votre texte du jour':
             self.title = st.title('Day text modification')
             self.WordOfDay = st.text_area('Modifier votre texte du jour :')
             button = st.button('Publier')
@@ -117,47 +109,59 @@ class Page:
                 st.success("texte updated with success")
             st.write('--------------------------------')
 
-        elif self.selection == 'Consulter vos texte':
-            self.title = st.title('Consult your text and emotions')
+    def df_all(self):
+
+        if self.side_selection == 'Suivit des emotions':
+            self.title = st.title('Consult texts and emotions passed')
             data = []
             result = self.sess.query(Text).filter_by().all()
             if result :
                 for text in self.user_text:
-                    data.append({'emotion_predicted' : text.emotion_predicted, 'texte' : text.content, 'time' : text.time_created, 'modif': text.time_updated})
+                    data.append({'emotion_predicted' : text.emotion_predicted, 'texte' : text.content, 'time' : text.time_created})
                 # st.dataframe(data_content, data_emotion)
                 st.dataframe(data)
             else:
                 st.write('Aucun texte enregistré pour le moment.')
 
-        elif self.selection == 'Consulter vos progressions':
 
+    def plot_graph(self):
+
+        if self.side_selection == 'Suivit des emotions':
+            self.title = st.title('Emotions pie and more')
             data = []
             if self.user_text :
                 for text in self.user_text:
-                    data.append({'emotion_predicted' : text.emotion_predicted, 'texte' : text.content, 'time' : text.time_created, 'modif': text.time_updated})
+                    data.append({'emotion_predicted' : text.emotion_predicted, 'texte' : text.content, 'time' : text.time_created})
                 df = pd.DataFrame(data)
-                self.title = st.title('Emotions tracker')
-                arr_2 = df[['emotion_predicted','texte']]
-                st.write(arr_2)
-                
+                df_pie = df[['emotion_predicted','texte']]
+                df_pie['emotion_int'] =  df.emotion_predicted.apply(lambda x : emotion_int(x))
+                df_pie = df_pie.groupby('emotion_predicted').count().reset_index()
+                fig1, ax1 = plt.subplots()
+                ax1.pie(df_pie.emotion_int, labels=df_pie.emotion_predicted, autopct='%1.1f%%',
+                        shadow=True, startangle=90)
+                ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+                col_graph1, col_graph2 = st.columns(2)
+                with col_graph1:
+                    st.pyplot(fig1)
+                st.write(df_pie)
                 arr = df.emotion_predicted
                 fig, ax = plt.subplots()
                 ax.hist(arr, bins=20)
                 plt.title('Bar of happyness')
-                st.pyplot(fig)
-
-
-
-
+                with col_graph2:
+                    st.pyplot(fig)
             else:
                 st.write('Aucun texte enregistré pour le moment.')
 
         st.markdown('by M.Zen coaching ')
 
     def run_page(self):
-        self.init_session()
-        self.session()
-        self.display_content()
+        self.login_streamlit()
+        self.instance_session()
+        self.add_content()
+        self.update_content()
+        self.df_all()
+        self.plot_graph()
 
 page = Page()
 page.run_page()
--- function.py
+++ function.py
@@ -1,8 +1,31 @@
 import requests
 import matplotlib.pyplot as plt
 import pandas as pd 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker 
 
 def predict_data(data):
 
     response = requests.post(f"http://0.0.0.0:8080/predict/?Data={data}")
     return response.text
+
+
+def make_engine_session():
+    engine = create_engine('sqlite:///feeling_db.sqlite3?check_same_thread=False')
+    Session = sessionmaker(bind=engine)
+
+    return Session()
+
+def emotion_int(result):
+    if result == '"sadness"' :
+        return 0
+    elif result == '"anger"' :
+        return 1
+    elif result == '"love"' :
+        return 2
+    elif result == '"surprise"' :
+        return 3
+    elif result == '"fear"' :
+        return 4
+    elif result == '"happy"' :
+        return 5
