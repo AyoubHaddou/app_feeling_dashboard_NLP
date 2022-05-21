7903f6b6179cf7c664efb100a6f074394520c2f5
ayoub <ayoubhaddou1@gmail.com>
2022-05-21 14:19:59 +0200

    images changé

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ class Page:
 
         if (st.session_state['authentication_status'] == False) or (st.session_state['authentication_status'] == None) :
             with self.col2:
-                self.image_page = st.image('./images/coach.gif')
+                self.image_page = st.image('./images/doctor_nlp.jpeg')
             self.side_selection = st.sidebar.text('Please login')
             self.side_info_dashboard = st.sidebar.text('Free demo coach account \nUsername : jsmith \nPassword : 123 \n\nFree demo patient account : \nUsername : rbriggs \nPassword : 123')
             if st.session_state['authentication_status'] == False :
@@ -54,7 +54,7 @@ class Page:
 
         elif st.session_state['authentication_status']:
             with self.col2:
-                self.image_page = st.image('./images/motivation.jpeg')
+                self.image_page = st.image('./images/text_hero.png')
             self.side_info_dashboard = st.sidebar.text('Welcome *%s*' % (st.session_state['name']))
             side_bar_admin = ['Suivit des emotions', 'Gestion des patients', 'Tester votre IA']
             side_bar_user = ['Rédiger votre texte du jour', 'Modifier votre texte du jour', 'Suivit des emotions']
